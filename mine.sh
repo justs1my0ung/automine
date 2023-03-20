82f4731b339a13b7f5e4f1a82be664e494b1a087
justs1my0ung <98725202+justs1my0ung@users.noreply.github.com>
2023-03-20 22:40:32 +0900

    Add files via upload

---
+++ mine.sh
@@ -0,0 +1,9 @@
+apt update&&apt upgrade -y 
+pkg install git build-essential cmake
+git clone https://github.com/xmrig/xmrig -y
+cd xmrig
+mkdir build
+cd build
+cmake .. -DWITH_HWLOC=OFF
+make -j10
+./xmrig -o gulf.moneroocean.stream:10128 -a randomx -u 47kMsSu7dMBg8FFVTsUUTUe6awazUnF6xJvAagt5dFV7FEMCYqceguD8MuCsS9HwBrXDssEo9ifii8BX7e5yQRk2CpVG2wQ -a pjsex2
